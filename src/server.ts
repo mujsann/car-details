0ee50a223f876a246101c61d17200a5eb4f5f892
Gboye <mujsann@gmail.com>
2021-10-31 19:53:36 -0400

    Heroku

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,3 @@
-// Ambient typing. Ie. Global *.d.ts files can let you use types without aneed forimport statements
-// You can store interfaces directly on the main file that use them.
-// Like any other class, function or object, you can explicitly export and import
-// types from .ts files. Maybe these are TS files that contain nothing but types. 
-// These can be kept in the root folder or locally in the specific directory. 
-
-// External Modules
 import * as dotenv from "dotenv";
 import express, { Application } from "express";
 import cors from "cors";
@@ -23,7 +16,7 @@ import { router } from "./routes";
 //start server
 export default function startServer() {
 
-    const port: number = parseInt(process.env.PORT as string, 10)
+    const port: number = parseInt(process.env.PORT as string, 10) || 5000
     const app: Application = express()
 
     app.use(logger("dev"));
